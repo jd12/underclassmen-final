f4ff94ea8c2308a9398a206ac07007397e8536db
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2017-06-12 19:33:45 -0700

    Initial Commit

---
+++ support/BlurbInterface.java
@@ -0,0 +1,25 @@
+public interface BlurbInterface {
+	
+	
+	/**
+	 * In the language of an alien race, all words take the form of Blurbs.
+	 * A Blurb is a Whoozit followed by one or more Whatzits. 
+	 * A Whoozit is the character ‘x’ followed by zero or more ‘y’s. 
+	 * A Whatzit is a ‘q’ followed by either a ‘z’ or a ‘d’, followed by a Whoozit. 
+	 * This method should be recursive
+	 * @return a valid Blurb
+	 */
+	public String BlurbGenerator();
+	
+	/**
+	 * Determines if {@code blurb} is a valid blurb as defined by {@code BlurbGenerator}
+	 * This method should be recursive 
+	 * @param blurb
+	 * 				the String to be tested
+	 * @return {@code true} if this {@code blurb} is a valid blurb and
+	 *         {@code false} otherwise.
+	 */
+	public boolean isBlurb(String blurb);
+	
+
+}
---
+++ support/CapitalGainsInterface.java
@@ -0,0 +1,10 @@
+
+public interface CapitalGainsInterface {
+	
+	public void buy(int shares, double price);
+	
+	public double sell(int shares, double price);
+	
+	public int getDay();
+
+}
